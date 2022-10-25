422a4d55d2058f8d06444b89d965d3d1b1f27c59
Qbik222 <97127218+Qbik222@users.noreply.github.com>
2022-10-25 13:37:19 +0300

    add animations

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,37 @@
 import * as flsFunctions from "./modules/functions.js";
  flsFunctions.isWebp();
+
+ const animItems = document.querySelectorAll("._anim-items");
+
+ if (animItems.length > 0){
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll(){
+        for (let i = 0; i < animItems.length; i++){
+            const animItem = animItems[i];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if(animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+                animItem.classList.add("_animation-active");
+            }else{
+                animItem.classList.remove("_animation-active");
+            }
+        }
+    }
+
+    function offset(el){
+        const rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+        return{ top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(()=>{animOnScroll();}, 500);
+    // animOnScroll();
+ }
